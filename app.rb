f12a12d7b382749011fb887373252eace44a3022
Samuel T <ssamt1142@gmail.com>
2019-06-19 02:48:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require_relative 'config/environment'
 require 'pry'
 
 class App < Sinatra::Base
-
   # This is a sample static route.
   get '/hello' do
     "Hello World!"
@@ -21,7 +20,6 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
-
     @num1 = params[:num1]
     @num2 = params[:num2]
     binding.pry
