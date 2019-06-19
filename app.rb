81fd2b5847b8169d6884fa72e0434c976bf954c7
Samuel T <ssamt1142@gmail.com>
2019-06-19 02:45:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -20,6 +21,7 @@ class App < Sinatra::Base
   end
 
   get '/multiply/:num1/:num2' do
+    binding.pry
     @num1 = params[:num1]
     @num2 = params[:num2]
     result = @num1*@num2
